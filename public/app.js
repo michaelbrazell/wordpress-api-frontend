8ecc1b2eac0f134622978d73c1a2bbb14fe1eeb0
Mike Murray <michaelbrazell@gmail.com>
2018-01-28 11:41:50 -0500

    add initial support to get featured images

--- public/app.js
+++ public/app.js
@@ -1,9 +1,31 @@
+// Create items
 function createNode(element) {
   return document.createElement(element);
 }
 
+// Attach items to dom
 function append(parent, el) {
   return parent.appendChild(el);
+}
+
+// Get featured image, not finished
+function returnMedia(mediaId) {
+  mediaUrl = `http://wordpress-api.local/wp-json/wp/v2/media/${mediaId}`;
+  if (mediaId != 0) {
+    fetch(mediaUrl)
+      .then((resp) => resp.json())
+      .then(function(mediaObject) {
+        console.log(mediaObject.guid.rendered)
+        let featuredItemUrl = mediaObject.guid.rendered;
+        mediaImg = createNode('img')
+        mediaImg.src = featuredItemUrl;
+        console.log(mediaImg);
+        //append(card.mediaImg);
+        // return mediaImg;
+      })
+  } else {
+    console.log('No Media for this item');
+  }
 }
 
 const postList = document.getElementById('post-list');
@@ -15,10 +37,11 @@ fetch(url)
     let posts = data;
     return posts.map(function(post) {
       let postItem = createNode('div'),
-          card = createNode('div');
+          card = createNode('div')
+      let getFeaturedItem = returnMedia(post.featured_media);
       postItem.className += "col-xs-12 col-sm-6 col-md-4 post-item mb-5 d-flex align-items-stretch";
       card.className += "card";
-      card.innerHTML = `<div class="card-body"><div class="card-title"><h4>${post.title.rendered}</h4></div><p class="card-text">${post.excerpt.rendered}</p><a href="${post.link}" class="btn btn-primary">Read more</a></div></a>`;
+      card.innerHTML = `<div class="card-body">${getFeaturedItem}<div class="card-title"><h4>${post.title.rendered}</h4></div><p class="card-text">${post.excerpt.rendered}</p><a href="${post.link}" class="btn btn-primary">Read more</a></div></a>`;
       append(postItem, card);
       append(postList, postItem);
     })
